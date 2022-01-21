571a935913cb09b2672f4560aaffc0596372f29c
Danilo Neves <daniloneves@aquileilao.onmicrosoft.com>
2022-01-20 21:08:11 -0300

    Ajuste no elemento de resultado

--- javascript.js
+++ javascript.js
@@ -1,27 +1,27 @@
 function soma(){
     var num1 = document.getElementById("num1").value
     var num2 = document.getElementById("num2").value
-    var result = parseInt(num1) + parseInt(num2)
-    document.getElementById("result").value = result
+    var result = parseInt(num1) + parseInt(num2)    
+    document.getElementById("result").innerText = result
 }
 
 function subtracao(){
     var num1 = document.getElementById("num1").value
     var num2 = document.getElementById("num2").value
     var result = parseInt(num1) - parseInt(num2)
-    document.getElementById("result").value = result
+    document.getElementById("result").innerText = result
 }
 
 function multiplicacao(){
     var num1 = document.getElementById("num1").value
     var num2 = document.getElementById("num2").value
     var result = parseInt(num1) * parseInt(num2)
-    document.getElementById("result").value = result
+    document.getElementById("result").innerText = result
 }
 
 function divisao(){
     var num1 = document.getElementById("num1").value
     var num2 = document.getElementById("num2").value
     var result = parseInt(num1) / parseInt(num2)
-    document.getElementById("result").value = result
+    document.getElementById("result").innerText = result
 }
